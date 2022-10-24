77f6999f342ebf9f2ffe2d30af827fd388d4d97e
Patrick <boahenepatrick2@gmail.com>
2022-10-24 12:34:28 +0000

    git demo

---
+++ Myclubwebsite/index.js
@@ -0,0 +1,17 @@
+
+    $(function(){
+
+
+
+        $("#send").click(function(){
+            
+            var username = $("#username").val()
+
+            var password = $("#password").val()
+
+            console.log("username",username)
+
+            console.log("password",password)
+        })
+
+    })
